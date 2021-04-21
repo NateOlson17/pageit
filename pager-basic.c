5ba64c9c759753d6e0b118385decd143a271b283
Nate Olson <nate.8olson@gmail.com>
2021-04-21 11:48:27 -0600

    redo basic pager

--- pager-basic.c
+++ pager-basic.c
@@ -17,42 +17,14 @@
  */
 
 #include "simulator.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void pageit(Pentry q[MAXPROCESSES]) { 
-    
-    /* Local vars */
-    int proc;
-    int pc;
-    int page;
-    int oldpage; 
-
-    /* Trivial paging strategy */
-    /* Select first active process */ 
-    for(proc=0; proc<MAXPROCESSES; proc++) { 
-	/* Is process active? */
-	if(q[proc].active) {
-	    /* Dedicate all work to first active process*/ 
-	    pc = q[proc].pc; 		        // program counter for process
-	    page = pc/PAGESIZE; 		// page the program counter needs
-	    /* Is page swaped-out? */
-	    if(!q[proc].pages[page]) {
-		/* Try to swap in */
-		if(!pagein(proc,page)) {
-		    /* If swapping fails, swap out another page */
-		    for(oldpage=0; oldpage < q[proc].npages; oldpage++) {
-	 		/* Make sure page isn't one I want */
-			if(oldpage != page) {
-			    /* Try to swap-out */
-			    if(pageout(proc,oldpage)) {
-				/* Break loop once swap-out starts*/
-				break;
-			    } 
-			}
-		    }
+void pageit(Pentry q[MAXPROCESSES]) {
+	for (int i = 0; i < MAXPROCESSES; ++i) {
+		int curpage = q[i].pc / PAGESIZE;
+		for (int j = 0; j < MAXPROCPAGES; ++j) {
+			(j == curpage) ? pagein(i,j) : pageout(i,j);
 		}
-	    }
-	    /* Break loop after finding first active process */
-	    break;
 	}
-    } 
-} 
+}
