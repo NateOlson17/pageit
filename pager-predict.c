c1ccf104f839cd8d9be5dbaf3728110b08f3599f
Nate Olson <nate.8olson@gmail.com>
2021-04-23 13:09:03 -0600

    clean up predicive implementation, still working

--- pager-predict.c
+++ pager-predict.c
@@ -18,29 +18,32 @@
 
 #include "simulator.h"
 
-void pageit(Pentry q[MAXPROCESSES]) { 
-    
+void pageit(Pentry q[MAXPROCESSES]) {
     /* This file contains the stub for a predictive pager */
     /* You may need to add/remove/modify any part of this file */
 
     /* Static vars */
     static int initialized = 0;
     static int tick = 1; // artificial time
-    
-    /* Local vars */
-    
+    static int timestamps[MAXPROCESSES][MAXPROCPAGES];
 
     /* initialize static vars on first run */
     if(!initialized){
 	/* Init complex static vars here */
-	
+	for (int i = 0; i < MAXPROCESSES; ++i) {
+	    for (int j = 0; j < MAXPROCPAGES; ++j) {
+		timestamps[i][j] = 0;
+	    }
+	}
 	initialized = 1;
     }
-    
-    /* TODO: Implement Predictive Paging */
-    fprintf(stderr, "pager-predict not yet implemented. Exiting...\n");
-    exit(EXIT_FAILURE);
+
+    for (int proc; proc < MAXPROCESSES; ++proc) { //loop through processes
+	if (q[proc].active) { //if process active
+	    
+	}
+    }
 
     /* advance time for next pageit iteration */
     tick++;
-} 
+}
